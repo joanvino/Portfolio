c924799f7143661a43b094f62c4076f3f9f9a18f
joanvino <vinopalebangan@gmail.com>
2021-12-16 07:50:17 +0800

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+    $(document).ready(function(){
+          $('ul li a').click(function(){
+            $('li a').removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+
+
+           
+
+
